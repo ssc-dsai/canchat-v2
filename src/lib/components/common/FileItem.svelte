24ff083c3262c0b426878eb75c3d625fb9a41ab5
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-03 21:10:33 -0700

    refac

--- src/lib/components/common/FileItem.svelte
+++ src/lib/components/common/FileItem.svelte
@@ -128,7 +128,7 @@
 	{/if}
 
 	{#if dismissible}
-		<div class=" pr-2">
+		<div class=" absolute -top-2 -right-2">
 			<button
 				class=" bg-white text-black border border-gray-50 rounded-full group-hover:visible invisible transition"
 				type="button"
@@ -136,7 +136,16 @@
 					dispatch('dismiss');
 				}}
 			>
-				<GarbageBin />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="w-4 h-4"
+				>
+					<path
+						d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
+					/>
+				</svg>
 			</button>
 
 			<!-- <button
