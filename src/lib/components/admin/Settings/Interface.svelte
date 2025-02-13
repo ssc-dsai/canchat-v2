efaa6feb2014b10191604a2bf590a85b139065da
Sam Mueller <sam.mueller@ssc-spc.gc.ca>
2025-02-13 13:56:11 -0500

    fix: uncomment button in Interface.svelte for future use

--- src/lib/components/admin/Settings/Interface.svelte
+++ src/lib/components/admin/Settings/Interface.svelte
@@ -357,7 +357,7 @@
 							{$i18n.t('Default Prompt Suggestions')}
 						</div>
 
-						<!-- <button
+						<button
 							class="p-1 px-3 text-xs flex rounded transition"
 							type="button"
 							on:click={() => {
@@ -379,7 +379,7 @@
 									d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
 								/>
 							</svg>
-						</button> -->
+						</button>
 					</div>
 					<div class="grid lg:grid-cols-2 flex-col gap-1.5">
 						{#each promptSuggestions as prompt, promptIdx}
