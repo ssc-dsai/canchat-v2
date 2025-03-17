6bdff46e0823b1d102ef97abb67dfce24459fe42
Sam Mueller <sam.mueller@ssc-spc.gc.ca>
2025-03-17 11:21:34 -0400

    refactor: improve formatting and readability of Help Button component

--- src/lib/components/layout/TopRightControls.svelte
+++ src/lib/components/layout/TopRightControls.svelte
@@ -90,9 +90,15 @@
 			</Tooltip>
 		{/if}
 
-			<!-- Help Button -->
+		<!-- Help Button -->
 		<div class="flex">
-			<button id="show-shortcuts-button" class="hidden" on:click={() => { showShortcuts = !showShortcuts; }} />
+			<button
+				id="show-shortcuts-button"
+				class="hidden"
+				on:click={() => {
+					showShortcuts = !showShortcuts;
+				}}
+			/>
 			<HelpMenu
 				showDocsHandler={() => {
 					window.open(getDocsUrl(), '_blank');
@@ -110,8 +116,19 @@
 					>
 						<div class="flex items-center justify-center">
 							<!-- Help Icon SVG -->
-							<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-900 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-								<path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-4 w-4 text-gray-900 dark:text-white"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								stroke-width="2"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+								/>
 							</svg>
 						</div>
 					</div>
@@ -131,8 +148,8 @@
 						on:show={(e) => {
 							if (e.detail === 'archived-chat') {
 								showArchivedChats.set(true);
-							}}
-						}
+							}
+						}}
 					>
 						<img
 							src={$user.profile_image_url}
