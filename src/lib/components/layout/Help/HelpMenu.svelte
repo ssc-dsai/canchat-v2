b1ed424b463b7bbe9947fceedadf7c976091d681
Pascal Beaulne <pascal.beaulne@gmail.com>
2025-03-31 11:41:09 -0400

    fix: typo

--- src/lib/components/layout/Help/HelpMenu.svelte
+++ src/lib/components/layout/Help/HelpMenu.svelte
@@ -83,7 +83,7 @@
 			</DropdownMenu.Item>
 			<DropdownMenu.Item
 				class="flex gap-2 items-center px-3 py-2 text-sm  cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md"
-				id="chat-share-button"
+				id="shortcut-button"
 				on:click={() => {
 					showShortcutsHandler();
 				}}
