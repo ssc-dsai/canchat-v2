bce2642a59322551bc128775bcef46994a717874
Justin Bertrand <justin.bertrand@ssc-spc.gc.ca>
2025-05-12 14:19:53 -0400

    chore: fmt.

--- backend/open_webui/migrations/versions/c76c3d700261_fix_message_metrics_created_at.py
+++ backend/open_webui/migrations/versions/c76c3d700261_fix_message_metrics_created_at.py
@@ -5,6 +5,7 @@ Revises: 634499cd1681
 Create Date: 2025-05-12 11:42:45.632822
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -13,8 +14,8 @@ from sqlalchemy.sql import table, column
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c76c3d700261'
-down_revision: Union[str, None] = '634499cd1681'
+revision: str = "c76c3d700261"
+down_revision: Union[str, None] = "634499cd1681"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -22,13 +23,16 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     message_metrics = table("message_metrics", column("created_at", sa.BigInteger))
     op.execute(
-        message_metrics.update()
-         .values({'created_at': message_metrics.c.created_at / 1000000000})
+        message_metrics.update().values(
+            {"created_at": message_metrics.c.created_at / 1000000000}
+        )
     )
+
 
 def downgrade() -> None:
     message_metrics = table("message_metrics", column("created_at", sa.BigInteger))
     op.execute(
-        message_metrics.update()
-         .values({'created_at': message_metrics.c.created_at * 1000000000})
+        message_metrics.update().values(
+            {"created_at": message_metrics.c.created_at * 1000000000}
+        )
     )
