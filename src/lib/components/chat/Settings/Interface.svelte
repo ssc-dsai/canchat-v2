8abaf8520228549420b4d9ac6351643493e3cc21
Pascal Beaulne <pascal.beaulne@gmail.com>
2025-09-04 13:55:41 -0400

    chore: fmt

--- src/lib/components/chat/Settings/Interface.svelte
+++ src/lib/components/chat/Settings/Interface.svelte
@@ -4,7 +4,6 @@
 	import { config, models, settings, user } from '$lib/stores';
 	import { createEventDispatcher, onMount, getContext } from 'svelte';
 	import { toast } from 'svelte-sonner';
-	import Tooltip from '$lib/components/common/Tooltip.svelte';
 	import { updateUserInfo } from '$lib/apis/users';
 	import { getUserPosition } from '$lib/utils';
 	const dispatch = createEventDispatcher();
@@ -805,28 +804,6 @@
 					</button>
 				</div>
 			</div>
-
-			<!-- <div>
-				<div class=" py-0.5 flex w-full justify-between">
-					<div class=" self-center text-xs">
-						{$i18n.t('Fluidly stream large external response chunks')}
-					</div>
-
-					<button
-						class="p-1 px-3 text-xs flex rounded-sm transition"
-						on:click={() => {
-							toggleSplitLargeChunks();
-						}}
-						type="button"
-					>
-						{#if splitLargeChunks === true}
-							<span class="ml-2 self-center">{$i18n.t('On')}</span>
-						{:else}
-							<span class="ml-2 self-center">{$i18n.t('Off')}</span>
-						{/if}
-					</button>
-				</div>
-			</div> -->
 
 			<div>
 				<div class=" py-0.5 flex w-full justify-between">
