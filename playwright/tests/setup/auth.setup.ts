10db9a99b7cfc80a55e3f2bc088c311ba3f9757d
YanSSC <yannick.gauthier@ssc-spc.gc.ca>
2026-01-06 13:30:08 -0500

    fix: enhance Playwright test setup with environment preparation and debug option

--- playwright/tests/setup/auth.setup.ts
+++ playwright/tests/setup/auth.setup.ts
@@ -32,11 +32,9 @@ setup('global setup: seed data & authenticate', async ({ page }) => {
 	await performAdminRegistration(page, authPage);
 	await seedUserAccounts(page, adminPage);
 	await saveAuthState(page, 'admin.json');
-	await adminPage.signOut();
-	await generateUserAuthFiles(page, authPage, adminPage);
 
 	// Make one model visible
-/* 	await adminPage.navigateToAdminSettings('Settings', 'Connections') //temporary
+ 	await adminPage.navigateToAdminSettings('Settings', 'Connections') //temporary
 	await adminPage.openModelSettings('chatgpt-4o-');
 	await adminPage.updateModelDescription({
 		en: 'English Description',
@@ -46,7 +44,9 @@ setup('global setup: seed data & authenticate', async ({ page }) => {
 	await adminPage.saveModelSettings();
 	await adminPage.updateChatModel('chatgpt-4o-latest');
 	await adminPage.setDefaultChatModel();
-	await adminPage.signOut(); */
+	await adminPage.signOut();
+
+	await generateUserAuthFiles(page, authPage, adminPage);
 
 	console.log('Global Setup Complete!');
 });
