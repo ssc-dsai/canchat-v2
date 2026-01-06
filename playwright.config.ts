51b4215b8d6c19a0e81bc0aec1ab621fbf5e5888
YanSSC <yannick.gauthier@ssc-spc.gc.ca>
2026-01-06 14:10:58 -0500

    fix: remove unnecessary environment variable unsetting in Playwright test setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
 	//retries: process.env.CI ? 2 : 0,
 	
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
-	//workers: 3,
+	//workers: process.env.CI ? 1 : undefined,
+	workers: 3,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
 		['html', { open: 'never' }],
