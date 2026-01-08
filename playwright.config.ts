7939ec3804f544a0df509e4fdb57cd90bf76ee9b
YanSSC <yannick.gauthier@ssc-spc.gc.ca>
2026-01-08 08:23:45 -0500

    fix: increase worker count in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 
 	/* Opt out of parallel tests on CI. */
 	//workers: process.env.CI ? 1 : undefined,
-	workers: 5,
+	workers: 10,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
 		['html', { open: 'never' }],
