c68f386f708d783754723d25be0ecf8d867db57e
YanSSC <yannick.gauthier@ssc-spc.gc.ca>
2026-01-08 09:26:21 -0500

    fix: update workflow name and adjust worker count in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,11 +17,11 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 
 	/* Retry on CI only */
-	//retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 2 : 0,
 
 	/* Opt out of parallel tests on CI. */
 	//workers: process.env.CI ? 1 : undefined,
-	workers: 10,
+	workers: 6,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
 		['html', { open: 'never' }],
