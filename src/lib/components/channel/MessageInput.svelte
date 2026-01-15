85e098c24c697878fcd5c304b59abe98ad5ffb58
Pascal Beaulne <pascal.beaulne@gmail.com>
2026-01-14 19:59:27 -0500

    fmt: removed unused dep

--- src/lib/components/channel/MessageInput.svelte
+++ src/lib/components/channel/MessageInput.svelte
@@ -14,7 +14,6 @@
 	import VoiceRecording from '../chat/MessageInput/VoiceRecording.svelte';
 	import InputMenu from './MessageInput/InputMenu.svelte';
 	import { uploadFile } from '$lib/apis/files';
-	import canchatAPI from '$lib/canchatAPI';
 	import FileItem from '../common/FileItem.svelte';
 	import Image from '../common/Image.svelte';
 	import { transcribeAudio } from '$lib/apis/audio';
