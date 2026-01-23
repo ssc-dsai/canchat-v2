b8063b3ad593e27d3340dc9d8817a5686cc362b4
Justin Bertrand <justin.bertrand@ssc-spc.gc.ca>
2026-01-23 15:58:30 -0500

    chore: fmt.

--- backend/open_webui/session/session_service_redis.py
+++ backend/open_webui/session/session_service_redis.py
@@ -49,7 +49,9 @@ class RedisSessionServiceImpl(RedisSessionService):
                     self._get_session_id(session.user_id),
                     session.model_dump_json(),
                 )
-                pipe.expire(self._get_session_id(session.user_id), self._session_lifetime)
+                pipe.expire(
+                    self._get_session_id(session.user_id), self._session_lifetime
+                )
                 await pipe.execute(raise_on_error=True)
             return True
         except redis.RedisError as e:
@@ -98,7 +100,9 @@ class RedisJSONSessionService(RedisSessionService):
                     Path.root_path(),
                     session.model_dump_json(),
                 )
-                pipe.expire(self._get_session_id(session.user_id), self._session_lifetime)
+                pipe.expire(
+                    self._get_session_id(session.user_id), self._session_lifetime
+                )
                 await pipe.execute(raise_on_error=True)
             return True
         except redis.RedisError as e:
