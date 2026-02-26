dcd4a1e3a0153690cec9ee21675265bb21d5079c
Pascal Beaulne <pascal.beaulne@gmail.com>
2026-02-26 12:46:14 -0500

    chore: fmt

--- backend/open_webui/migrations/versions/5a92423d379e_remove_memory.py
+++ backend/open_webui/migrations/versions/5a92423d379e_remove_memory.py
@@ -5,14 +5,14 @@ Revises: 9503a87090c4
 Create Date: 2026-02-26 12:41:17.844406
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
-revision: str = '5a92423d379e'
-down_revision: Union[str, None] = '9503a87090c4'
+revision: str = "5a92423d379e"
+down_revision: Union[str, None] = "9503a87090c4"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
