dcd397a68268845f8097faac916a57fd7f5ea86e
YanSSC <yannick.gauthier@ssc-spc.gc.ca>
2026-03-05 13:24:35 -0500

    test(playwright): added sidebar test and improved execution (#488)
    * Stubs for all test cases and implementation for test case 1, not ran yet
    
    * feat(playwright): added test 1-3 & updated searInput.svelte design
    
    * fix(chat-folder): fixed the chat folder so a conversation can be selected
    
    * chore:fmt
    
    * test(playwright): Adding more test for sidebar
    
    * test(playwright): improve test robutness
    
    * fix(playwright): skipping image generation test because dall-e is disabled
    
    * test(playwright): updated CI logic to merge properly
    
    * i18n(locales/fr-CA): adding missing translation
    
    * test(playwright): fixed review issues
    
    ---------
    
    Co-authored-by: jeneerthan <jeneerthan7@gmail.com>

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -932,6 +932,8 @@
 				{#if !search && folders}
 					<Folders
 						{folders}
+						{selectedChatIds}
+						showBulkActions={selectedChatIds.length > 0}
 						on:import={(e) => {
 							const { folderId, items } = e.detail;
 							importChatHandler(items, false, folderId);
@@ -941,6 +943,16 @@
 						}}
 						on:change={async () => {
 							initChatList();
+						}}
+						on:select={(e) => {
+							toggleChatSelection(e.detail);
+						}}
+						on:unselect={(e) => {
+							toggleChatSelection(e.detail);
+						}}
+						on:tag={(e) => {
+							const { type, name, chatId } = e.detail;
+							tagEventHandler(type, name, chatId);
 						}}
 					/>
 				{/if}
